6a32532bb0b92519d258cee339e2cd8a0a6564f9
LuisTS <info@proto-web.net>
2018-12-01 18:24:40 +0100

    Sección 7 lista

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCC8lzIS54oKiHEQu_tg04wDr1BX1CCQKk',
+    authDomain: 'udemy-angular-ingreso-egreso.firebaseapp.com',
+    databaseURL: 'https://udemy-angular-ingreso-egreso.firebaseio.com',
+    projectId: 'udemy-angular-ingreso-egreso',
+    storageBucket: 'udemy-angular-ingreso-egreso.appspot.com',
+    messagingSenderId: '425961262048'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCC8lzIS54oKiHEQu_tg04wDr1BX1CCQKk',
+    authDomain: 'udemy-angular-ingreso-egreso.firebaseapp.com',
+    databaseURL: 'https://udemy-angular-ingreso-egreso.firebaseio.com',
+    projectId: 'udemy-angular-ingreso-egreso',
+    storageBucket: 'udemy-angular-ingreso-egreso.appspot.com',
+    messagingSenderId: '425961262048'
+  }
 };
 
 /*
