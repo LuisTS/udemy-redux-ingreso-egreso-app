3a7772915276f5d07925afe1e6b9dc4491596e30
LuisTS <info@proto-web.net>
2018-12-01 21:43:49 +0100

    Sección 8 lista - inicio redux

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,28 +3,48 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 
+import { Store } from '@ngrx/store';
+import { ActivarLoadingAction, DesactivarLoadingAction } from '../shared/ui.accions';
+import { AppState } from '../app.reducer';
+
 import * as firebase from 'firebase';
 import { map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 import Swal from 'sweetalert2';
 import { User } from './user.model';
+import { SetUserAction } from './auth.actions';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private userSubscription: Subscription = new Subscription();
+
   constructor(private afAuth: AngularFireAuth,
               private router: Router,
-              private afDB: AngularFirestore) { }
+              private afDB: AngularFirestore,
+              private store: Store<AppState>) { }
 
   initAuthListener() {
     this.afAuth.authState.subscribe((fbUser: firebase.User) => {
-      console.log(fbUser);
+      if (fbUser) {
+        this.userSubscription = this.afDB.doc(`${fbUser.uid}/usuario`).valueChanges()
+        .subscribe((usuarioObj: any) => {
+          const newUser = new User(usuarioObj);
+          this.store.dispatch(new SetUserAction(newUser));
+        });
+      } else {
+        this.userSubscription.unsubscribe();
+      }
     });
   }
 
   crearUsuario(nombre: string, email: string, password: string) {
+
+    this.store.dispatch(new ActivarLoadingAction());
+
     this.afAuth.auth.createUserWithEmailAndPassword(email, password)
     .then(resp => {
 
@@ -37,20 +57,27 @@ export class AuthService {
       this.afDB.doc(`${user.uid}/usuario`).set(user)
       .then(() => {
         this.router.navigate(['/']);
+        this.store.dispatch(new DesactivarLoadingAction());
       });
 
     })
     .catch(error => {
+      this.store.dispatch(new DesactivarLoadingAction());
       Swal('Error en el registro', error.message, 'error');
     });
   }
 
   login(email: string, password: string) {
+
+    this.store.dispatch(new ActivarLoadingAction());
+
     this.afAuth.auth.signInWithEmailAndPassword(email, password)
     .then(resp => {
+      this.store.dispatch(new DesactivarLoadingAction());
       this.router.navigate(['/']);
     })
     .catch(error => {
+      this.store.dispatch(new DesactivarLoadingAction());
       Swal('Error en el login', error.message, 'error');
     });
   }
